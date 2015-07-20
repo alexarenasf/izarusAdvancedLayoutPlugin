6ea5ea2baae826b8e4f885f3f61020b5e3481f28
Alex Arenas <alex@izarus.cl>
2015-07-20 02:13:21 -0300

    Fix cero permisos

--- lib/advancedLayout/advancedLayoutFilter.class.php
+++ lib/advancedLayout/advancedLayoutFilter.class.php
@@ -3,7 +3,7 @@ class advancedLayoutFilter extends sfFilter
 {
   public function execute($filterChain)
   {
-    if ($this->isFirstCall() && $this->getContext()->getUser()->isAuthenticated() && AdvancedLayout::isModuleAdvanced())
+    if ($this->isFirstCall() && $this->getContext()->getUser()->isAuthenticated())
     {
       $profiles = $this->getContext()->getUser()->getGuardUser()->getPermissions();
       if(!count($profiles)){
@@ -15,21 +15,23 @@ class advancedLayoutFilter extends sfFilter
         AdvancedLayout::setCurrentProfile($profiles[0]->getId());
       $profile = AdvancedLayout::getCurrentProfile();
       
-      $action = $this->getContext()->getActionName(); 
-      if(!$profile && $action <> 'selectProfile'){
-        $this->getContext()->getController()->forward('izarusAdvancedCustomize','selectProfile');
-        throw new sfStopException();
-      }else{
-        $polymorfism = AdvancedLayout::modulePolymorfism();
-        
-        if(!AdvancedLayout::userHasPermission()){
-          $this->getContext()->getController()->forward('izarusAdvancedCustomize','forbidden');
+      if(AdvancedLayout::isModuleAdvanced()){
+        $action = $this->getContext()->getActionName(); 
+        if(!$profile && $action <> 'selectProfile'){
+          $this->getContext()->getController()->forward('izarusAdvancedCustomize','selectProfile');
           throw new sfStopException();
-        }else if($polymorfism!==false){
-          $this->getContext()->getController()->forward($polymorfism['module'],$polymorfism['action']);
-          throw new sfStopException();
-        }        
-      }      
+        }else{
+          $polymorfism = AdvancedLayout::modulePolymorfism();
+          
+          if(!AdvancedLayout::userHasPermission()){
+            $this->getContext()->getController()->forward('izarusAdvancedCustomize','forbidden');
+            throw new sfStopException();
+          }else if($polymorfism!==false){
+            $this->getContext()->getController()->forward($polymorfism['module'],$polymorfism['action']);
+            throw new sfStopException();
+          }        
+        }   
+      }
     }
     $filterChain->execute();
   }
